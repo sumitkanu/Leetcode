99e177e3d89aeca4cc92a1cc5042a365d5c81060
sumitkanu <35460954+sumitkanu@users.noreply.github.com>
2025-02-22 01:05:58 -0500

    Time: 4 ms (61.82%) | Memory: 56.4 MB (43.32%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.java
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -8,16 +8,13 @@ class Solution {
             sum += nums[end];
             end++;
         }
-        System.out.println(sum);
         double max = sum/k;
 
         while(end < nums.length) {
-            System.out.println(end);
             sum -= nums[start];
             sum += nums[end];
             start++;
             end++;
-            System.out.println(sum);
             max = max > (sum/k) ? max : sum/k;
         }
 
