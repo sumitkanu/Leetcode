419c295d5b1e1d3d57e4bf7993b9b46110fcc30d
sumitkanu <35460954+sumitkanu@users.noreply.github.com>
2025-02-24 15:39:39 -0500

    Time: 11 ms (44.59%) | Memory: 45.6 MB (54.46%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
@@ -1,27 +1,26 @@
 class Solution {
-    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
-        List<List<Integer>> answer = new ArrayList<>();
-        Set<Integer> arr1 = new HashSet<>();
-        Set<Integer> arr2 = new HashSet<>();
-
-        for(int num : nums1) {
-            arr1.add(num);
+    // Returns the elements in the first arg nums1 that don't exist in the second arg nums2.
+    List<Integer> getElementsOnlyInFirstList(int[] nums1, int[] nums2) {
+        Set<Integer> onlyInNums1 = new HashSet<> (); 
+        
+        // Store nums2 elements in an unordered set. 
+        Set<Integer> existsInNums2 = new HashSet<> (); 
+        for (int num : nums2) {
+            existsInNums2.add(num);
         }
-
-        for(int num: nums2) {
-            arr2.add(num);
-            if(arr1.contains(num)) {
-                arr1.remove(num);
+        
+        // Iterate over each element in the list nums1.
+        for (int num : nums1) {
+            if (!existsInNums2.contains(num)) {
+                onlyInNums1.add(num);
             }
         }
         
-        for(int num : nums1) {
-            if(arr2.contains(num)) arr2.remove(num);
-        }
-
-        answer.add(new ArrayList<>(arr1));
-        answer.add(new ArrayList<>(arr2));
-
-        return answer;
+        // Convert to vector.
+        return new ArrayList<>(onlyInNums1);
+    }
+    
+    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        return Arrays.asList(getElementsOnlyInFirstList(nums1, nums2), getElementsOnlyInFirstList(nums2, nums1));
     }
 }
