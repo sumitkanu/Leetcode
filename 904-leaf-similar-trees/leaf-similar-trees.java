7447dd6d6d92be8cccb38abd153954359abee494
sumitkanu <35460954+sumitkanu@users.noreply.github.com>
2025-02-24 19:45:24 -0500

    Time: 7 ms (4.31%) | Memory: 41.7 MB (29.49%) - LeetSync

---
+++ 904-leaf-similar-trees/leaf-similar-trees.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> arr1 = new ArrayList<>();
+        List<Integer> arr2 = new ArrayList<>();
+        leafList(root1, arr1);
+        leafList(root2, arr2);
+        for(int num: arr1){
+            System.out.println(num);
+        }
+        for(int num: arr2){
+            System.out.println(num);
+        }
+
+        if(arr1.size() != arr2.size()) return false;
+
+        System.out.print("Here");
+        for(int i=0; i<arr1.size(); i++) {
+            if (!arr1.get(i).equals(arr2.get(i))) return false;
+        }
+
+        System.out.print("Here1");
+
+        return true;
+    }
+
+    private void leafList(TreeNode root, List<Integer> list) {
+        if(root.left == null && root.right == null) {
+            list.add(root.val);
+            return;
+        }
+
+        if(root.left != null) leafList(root.left, list);
+        if(root.right != null) leafList(root.right, list);
+    }
+}
