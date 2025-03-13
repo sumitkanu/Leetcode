c744e62ae99794d40a4b2e4df098145cf7e8bb30
sumitkanu <35460954+sumitkanu@users.noreply.github.com>
2025-03-12 21:15:25 -0400

    Time: 9 ms (99.20%) | Memory: 44.9 MB (99.90%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -1,28 +1,20 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
-        char[] charArr1 = word1.toCharArray();
-        char[] charArr2 = word2.toCharArray();
-
-        Arrays.sort(charArr1);
-        Arrays.sort(charArr2);
-        
-        if(Arrays.equals(charArr1, charArr2))
-            return true;
-        
-        if(charArr1.length != charArr2.length)
-            return false;
-
         int[] word1Freq = new int[26];
         int[] word2Freq = new int[26];
 
-        for(int i = 0; i < charArr1.length; i++) {
-            word1Freq[charArr1[i] - 'a'] += 1;
-            word2Freq[charArr2[i] - 'a'] += 1;
+        for (char c : word1.toCharArray()) {
+            word1Freq[c - 'a']++;
+        }
+        for (char c : word2.toCharArray()) {
+            word2Freq[c - 'a']++;
         }
 
-        for(int i = 0; i < word1Freq.length; i++) {
-            if(word1Freq[i] == 0 && word2Freq[i] != 0)
+        for (int i = 0; i < 26; i++) {
+            if ((word1Freq[i] == 0 && word2Freq[i] > 0) ||
+                (word2Freq[i] == 0 && word1Freq[i] > 0)) {
                 return false;
+            }
         }
 
         Arrays.sort(word1Freq);
