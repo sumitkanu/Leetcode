6cae15da1d2b4fcdf91e964b6b474fbff2ee46da
sumitkanu <35460954+sumitkanu@users.noreply.github.com>
2025-03-16 22:37:23 -0400

    Time: 7 ms (47.99%) | Memory: 45.6 MB (33.89%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,32 +1,40 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> st = new Stack<Integer>();
 
-        for(int i = 0; i < asteroids.length; i++) {
-            if(stack.isEmpty() || stack.peek() < 0 || asteroids[i] > 0){
-                stack.push(asteroids[i]);
-            } else {
-                int popped = 2000;
-                while(!stack.isEmpty() && stack.peek() > 0) {
-                    if(stack.peek() < -asteroids[i]) {
-                        popped = stack.pop();
-                    } else break;
+        for (int asteroid : asteroids) {
+            boolean flag = true;
+            while (!st.isEmpty() && (st.peek() > 0 && asteroid < 0)) {
+                // If the top asteroid in the stack is smaller, then it will explode.
+                // Hence pop it from the stack, also continue with the next asteroid in the stack.
+                if (Math.abs(st.peek()) < Math.abs(asteroid)) {
+                    st.pop();
+                    continue;
                 }
-                
-                if(!stack.isEmpty() && stack.peek() == -asteroids[i]) {
-                    stack.pop();
-                } else if (stack.isEmpty() || (!stack.isEmpty() && stack.peek() < -asteroids[i])) {
-                    stack.push(asteroids[i]);
+                // If both asteroids have the same size, then both asteroids will explode.
+                // Pop the asteroid from the stack; also, we won't push the current asteroid to the stack.
+                else if (Math.abs(st.peek()) == Math.abs(asteroid)) {
+                    st.pop();
                 }
+
+                // If we reach here, the current asteroid will be destroyed
+                // Hence, we should not add it to the stack
+                flag = false;
+                break;
+            }
+
+            if (flag) {
+                st.push(asteroid);
             }
         }
 
-        int[] ans = new int[stack.size()];
-        for (int i = ans.length - 1; i >= 0; i--) {
-            ans[i] = stack.peek();
-            stack.pop();
+        // Add the asteroids from the stack to the vector in the reverse order.
+        int[] remainingAsteroids = new int[st.size()];
+        for (int i = remainingAsteroids.length - 1; i >= 0; i--) {
+            remainingAsteroids[i] = st.peek();
+            st.pop();
         }
 
-        return ans;
+        return remainingAsteroids;
     }
 }
