8019e100dca018894a3367a774c0de35aeb37725
sumitkanu <35460954+sumitkanu@users.noreply.github.com>
2025-04-26 20:56:58 -0400

    Time: 143 ms (61.25%) | Memory: 41.7 MB (50.47%) - LeetSync

---
+++ 79-word-search/word-search.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        for(int  i=0; i<board.length; i++)
+            for(int j=0; j<board[0].length; j++)
+                if(board[i][j] == word.charAt(0) && valid(board, word, 0, i, j))
+                    return true;
+
+        return false;
+    }
+
+    private boolean valid(char[][] board, String word, int n, int x, int y) {
+        if(n >= word.length()) return true;
+
+        if(x >= board.length || y >= board[0].length || x < 0 || y < 0 || board[x][y] != word.charAt(n)) return false;
+
+        boolean found;
+        char temp = board[x][y];
+
+        board[x][y] = '#';
+
+        found = valid(board, word, n+1, x+1, y) || valid(board, word, n+1, x-1, y) ||
+                valid(board, word, n+1, x, y+1) || valid(board, word, n+1, x, y-1);
+
+        if(found) {
+            System.out.print(x);
+            System.out.print(" ");
+            System.out.println(y);
+        }
+        
+        board[x][y] = temp;
+
+        return found;
+    }
+}
