2c947f0f9a86b9212de3bca96dd59cdb008d42aa
sumitkanu <35460954+sumitkanu@users.noreply.github.com>
2025-05-30 19:51:34 -0400

    Time: 63 ms (10.35%) | Memory: 45.7 MB (24.01%) - LeetSync

---
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minDeletions(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        Map<Integer, Integer> mapInt = new HashMap<>();
+        int count = 0;
+
+        for(int i=0; i<s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0)+1);
+        }
+
+        for(int val : map.values()) {
+            System.out.println(val);
+            while(val > 0 && mapInt.containsKey(val)) {
+                val--;
+                count++;
+            } 
+            mapInt.put(val,1);
+        }
+
+        return count;
+    }
+}
