2eeeb9271a991fe467c8079e3793586f71b5ffea
sumitkanu <35460954+sumitkanu@users.noreply.github.com>
2025-05-30 19:33:18 -0400

    Time: 0 ms (100.00%) | Memory: 40.9 MB (94.74%) - LeetSync

--- 1247-decrease-elements-to-make-array-zigzag/decrease-elements-to-make-array-zigzag.java
+++ 1247-decrease-elements-to-make-array-zigzag/decrease-elements-to-make-array-zigzag.java
@@ -2,34 +2,23 @@ class Solution {
     public int movesToMakeZigzag(int[] nums) {
         if(nums.length == 1) return 0;
         int moves = 0;
-
-        for(int i = 0; i < nums.length; i += 2) {
-            if(i == 0 && nums[0] >= nums[1]) {
-                moves += nums[0] - nums[1] + 1;
-            } else if(i == nums.length - 1 && nums[i] >= nums[i-1]) {
-                moves += nums[i] - nums[i-1] + 1;
-            } else if(i>0 && i<nums.length-1){
-                int num = Math.min(nums[i+1], nums[i-1]);
-                if(nums[i] >= num) {
-                    moves += nums[i] - num + 1;
-                }
-            }
-        }
-
         int oddmoves = 0;
 
-        for(int i = 1; i < nums.length; i += 2) {
-
-            if(i == nums.length - 1 && nums[i] >= nums[i-1]) {
-                oddmoves += nums[i] - nums[i-1] + 1;
+        for(int i = 0; i < nums.length; i += 1) {
+            if(i == 0 && nums[0] >= nums[1]) {
+                if(i%2 == 0) moves += nums[0] - nums[1] + 1;
+                else oddmoves += nums[0] - nums[1] + 1;
+            } else if(i == nums.length - 1 && nums[i] >= nums[i-1]) {
+                if(i%2 == 0) moves += nums[i] - nums[i-1] + 1;
+                else oddmoves += nums[i] - nums[i-1] + 1;
             } else if(i>0 && i<nums.length-1){
                 int num = Math.min(nums[i+1], nums[i-1]);
                 if(nums[i] >= num) {
-                    oddmoves += nums[i] - num + 1;
+                    if(i%2 == 0) moves += nums[i] - num + 1;
+                    else oddmoves += nums[i] - num + 1;
                 }
             }
         }
-
         return Math.min(oddmoves,moves);
     }
 }
