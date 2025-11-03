942e6aab4ee41966e72d7f22a55539516d2b7432
sumitkanu <35460954+sumitkanu@users.noreply.github.com>
2025-11-02 22:57:20 -0500

    Time: 5 ms (12.24%) | Memory: 43.5 MB (5.02%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -3,7 +3,7 @@ class Solution {
         Stack<Character> stack = new Stack<>();
         for(int i = 0; i<=s.length()-1; i++){
             if(s.charAt(i) == '(' || s.charAt(i) == '{' ||s.charAt(i) == '['){
-                stack.add(s.charAt(i));
+                stack.push(s.charAt(i));
             } else if(s.charAt(i) == ')') {
                 if(stack.empty() || stack.pop() != '('){
                     return false;
