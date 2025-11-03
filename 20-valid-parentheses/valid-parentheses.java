8da22793d3933474d95da166a022b81922a3664f
sumitkanu <35460954+sumitkanu@users.noreply.github.com>
2025-11-02 22:56:45 -0500

    Time: 4 ms (27.39%) | Memory: 43.8 MB (5.02%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i<=s.length()-1; i++){
+            if(s.charAt(i) == '(' || s.charAt(i) == '{' ||s.charAt(i) == '['){
+                stack.add(s.charAt(i));
+            } else if(s.charAt(i) == ')') {
+                if(stack.empty() || stack.pop() != '('){
+                    return false;
+                }
+            } else if(s.charAt(i) == '}') {
+                if(stack.empty() || stack.pop() != '{'){
+                    return false;
+                }
+            } else if(s.charAt(i) == ']') {
+                if(stack.empty() || stack.pop() != '['){
+                    return false;
+                }
+            }
+        }
+        if(stack.empty()) return true;
+        return false;
+    }
+}
