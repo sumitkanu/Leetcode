40d7515af7375a93c4a3a1fb93c26cf6b814a2ed
sumitkanu <35460954+sumitkanu@users.noreply.github.com>
2026-01-08 12:08:01 -0500

    Time: 3 ms (42.38%) | Memory: 23.5 MB (6.63%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -7,8 +7,6 @@ class Solution:
         end = len(s) - 1
 
         while(start <= end):
-            temp = s[start]
-            s[start] = s[end]
-            s[end] = temp
+            s[start],s[end]=s[end],s[start]
             start += 1
             end -= 1
