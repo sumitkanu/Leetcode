88aa20a7cb061b82bbea22ec27f4d697b7fba8af
sumitkanu <35460954+sumitkanu@users.noreply.github.com>
2026-03-26 01:08:13 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (98.84%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -4,29 +4,16 @@ class Solution:
         n = len(matrix[0])
 
         start = 0
-        end = m - 1
+        end = (m * n) - 1
 
         while start <= end:
             mid = (start + end) // 2
 
-            if matrix[mid][0] == target:
+            if matrix[mid // n][mid % n] == target:
                 return True
-            elif matrix[mid][0] < target:
+            elif matrix[mid // n][mid % n] < target:
                 start = mid + 1
             else:
                 end = mid - 1
 
-        i = end
-        start = 0
-        end = n - 1
-        while start <= end:
-            mid = (start + end) // 2
-
-            if matrix[i][mid] == target:
-                return True
-            if matrix[i][mid] > target:
-                end = mid - 1
-            else:
-                start = mid + 1
-        
         return False
