e1ce5fdaaea8b71c535afd6724040e48d8c09c5c
sumitkanu <35460954+sumitkanu@users.noreply.github.com>
2026-04-16 11:22:47 -0400

    Time: 52 ms (80.55%) | Memory: 29.6 MB (72.44%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -4,14 +4,11 @@ class Solution:
         start = 0
         end = len(height) - 1
 
-        max_area = (end - start) * min(height[start], height[end])
-
         while start < end:
-            if height[end] <= height[start]:
+            max_area = max(max_area, (end - start) * min(height[start], height[end]))
+            if height[start] >= height[end]:
                 end -= 1
-                max_area = max(max_area, (end - start) * min(height[start], height[end]))
             else:
                 start += 1
-                max_area = max(max_area, (end - start) * min(height[start], height[end]))
 
         return max_area
