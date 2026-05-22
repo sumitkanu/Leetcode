7d08ed92c5b62ac900d052eef4ae63f2a3ca7b5d
sumitkanu <35460954+sumitkanu@users.noreply.github.com>
2026-05-21 20:25:20 -0400

    Time: 3 ms (10.96%) | Memory: 19.5 MB (41.39%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.py
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.py
@@ -9,25 +9,31 @@ class Solution:
         """
         Do not return anything, modify root in-place instead.
         """
-        def dfs(root):
-            if not root:
+        def findLast(root):
+            while root:
+                if root.right:
+                    root = root.right
+                    continue
+                elif root.left:
+                    root = root.left
+                    continue
                 return root
 
-            if not root.left and not root.right:
-                return root
 
+
+        while root:
             right = root.right
+
             if root.left:
                 root.right = root.left
                 root.left = None
-                last = dfs(root.right)
             else:
-                last = root
-
-            if right and last:
-                last.right = right
-                return dfs(right)
+                root = root.right
+                continue
             
-            return last
+            if root.right:
+                last = findLast(root.right)
+                if last:
+                    last.right = right
 
-        dfs(root)
+            root = root.right
