0cb66ecb88e1a955079cca6caac4fc69ad50415d
sumitkanu <35460954+sumitkanu@users.noreply.github.com>
2026-05-21 21:05:15 -0400

    Time: 7 ms (77.66%) | Memory: 19.5 MB (13.50%) - LeetSync

---
+++ 38-count-and-say/count-and-say.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        ans = "1"
+        for num in range(2, n + 1):
+            print(ans)
+            temp = ""
+            curr = ans[0]
+            count = 0
+            for char in ans:
+                if char == curr:
+                    count += 1
+                else:
+                    temp = temp + str(count) + curr
+                    curr = char
+                    count = 1
+
+            temp = temp + str(count) + curr
+            ans = temp
+
+        return ans
