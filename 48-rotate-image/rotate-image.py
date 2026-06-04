325ce5b50f50163826b95efcc626950c606ca277
sumitkanu <35460954+sumitkanu@users.noreply.github.com>
2026-06-03 23:20:59 -0400

    Time: 0 ms (100.00%) | Memory: 19.2 MB (69.07%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -9,5 +9,4 @@ class Solution:
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
         for i in range(n):
-            for j in range(n // 2):
-                matrix[i][j], matrix[i][n - j - 1] = matrix[i][n - j - 1], matrix[i][j]
+            matrix[i].reverse()
